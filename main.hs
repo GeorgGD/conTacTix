37427843a3ea742f39e8349dcc4db907fcca33e7
GeorgGD <36535082+GeorgGD@users.noreply.github.com>
2019-02-20 12:59:19 +0100

    sorting board by rows
    Added functions sortBoard, sortBoardByRow and removeRow to make it easier to search through the list.

--- main.hs
+++ main.hs
@@ -1,9 +1,8 @@
 {-
-
-
 -}
 
-data Point = Empty (Int,Int) | White (Int,Int) | Black (Int,Int) deriving (Show)
+data Point = Empty
+ (Int,Int) | White (Int,Int) | Black (Int,Int) deriving (Show)
 
 
 
@@ -16,9 +15,6 @@ type Board = [Point]
   Creates an unoccupied point with the given co-ordinates (a,b)
   RETURNS: The empty point with coordinates (a,b)
   EXAMPLES: makePoint 1 1 = Empty (1,1)
-
-
-
 -}
 makePoint :: Int -> Int -> Point
 makePoint a b = Empty (a,b)
@@ -28,8 +24,6 @@ makePoint a b = Empty (a,b)
   makeBoard x
   creates a Board of size x^2
   RETURNS: Empty
-
-
 -}
 
 makeBoard :: Int -> Board
@@ -44,20 +38,25 @@ colmBoard :: Int -> Int -> Int -> Board
 colmBoard x a b | mod b x == (x-1) = [makePoint a b]
                 | mod b x  < (x-1) = [makePoint a b] ++ (colmBoard x a (b+1))
 
+sortBoard :: Board -> [Board]
+sortBoard [] = []
+sortBoard (x:xs) = [sortBoardByRow(x:xs)] ++ sortBoard(removeRow x (x:xs))
+                              
+comparePoints :: Point -> Point -> Bool
+comparePoints (Empty (a1,_)) (Empty (a2,_)) | a1 == a2 = True
+                                            | otherwise = False
+                                            
+sortBoardByRow :: Board -> Board
+sortBoardByRow [] = []
+sortBoardByRow (x:xs) | length xs == 0 = [x]
+                      | comparePoints x (head xs) == True = x:(sortBoardByRow xs)
+                      | comparePoints x (head xs) == False = [x] 
 
-makeMove :: Point -> Board
-makeMove (White (a,b)) = [Empty (0,0)]
-makeMove (Black (a,b)) = [Empty (1,2)]
-
-
-
-{-
-  emptyPoint p
-  checks if p is empty
-  RETURNS: True if p is empty, otherwise False
-  EXAMPLES: emptyPoint (Empty (1,2)) == True
-            emptyPoint (White (3,3)) == False
--}
+removeRow :: Point -> Board -> Board
+removeRow _ [] = []
+removeRow k (x:xs) | comparePoints k x == True = removeRow k xs
+                   | comparePoints k x == False = (x:xs)
+    
 
 emptyPoint :: Point -> Bool
 emptyPoint (Empty (a,b)) = True
